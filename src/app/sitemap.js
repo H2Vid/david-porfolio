80a1e294b9c06fc7bf70f0d5008d3204766bf949
H2Vid <dstmpl17@gmail.com>
2026-09-13 15:40:24 +0700

    delete icon unused

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://itsviids.my.id",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://itsviids.my.id/projects",
+      lastModified: new Date(),
+    },
+  ];
+}
